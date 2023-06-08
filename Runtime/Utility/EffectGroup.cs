9ca96b10de86ec5dd6c1709e8b8e7f24c7ecf2a6
Miki <miki@macaca.games>
2023-06-08 19:46:43 +0800

    remove litjson dependency

--- Runtime/Utility/EffectGroup.cs
+++ Runtime/Utility/EffectGroup.cs
@@ -6,7 +6,7 @@ using MacacaGames.EffectSystem.Model;
 
 namespace MacacaGames.EffectSystem
 {
-    [CreateAssetMenu(menuName = "Data/Effect/EffectGroup", fileName = "EffectGroup")]
+    [CreateAssetMenu(menuName = "Data/EffectSystem/EffectGroup", fileName = "EffectGroup")]
     public class EffectGroup
 #if UNITY_EDITOR || UNITY_ANDROID || UNITY_IOS
     : ScriptableObject
@@ -17,7 +17,6 @@ namespace MacacaGames.EffectSystem
         [SerializeField]
         public string title;
 
-        [TextArea(1, 5)]
         [SerializeField]
         public string description;
 
@@ -31,54 +30,14 @@ namespace MacacaGames.EffectSystem
             return name;
         }
 
-        // public override IEnumerable<IQueryableData> GetData()
-        // {
-        //     return effects.OfType<IQueryableData>();
-        // }
-
-
-
-        [Sirenix.OdinInspector.Button]
-        public void GetLitJsoneffects()
-        {
-            // Debug.Log(Newtonsoft.Json.JsonConvert.SerializeObject(effects));
-
-            Debug.Log(LitJson.JsonMapper.ToJson(effects));
-        }
-        [Sirenix.OdinInspector.Button]
         public void GetJsonNeteffects()
         {
             Debug.Log(Newtonsoft.Json.JsonConvert.SerializeObject(effects));
-
         }
-        [Sirenix.OdinInspector.Button]
-        public void FromJson(string test)
-        {
-            var result = LitJson.JsonMapper.ToObject<List<EffectInfo>>(test);
-
-            Debug.Log(result);
-        }
-        [Sirenix.OdinInspector.Button]
+      
         public void FromJsonNet(string test)
         {
             var result = Newtonsoft.Json.JsonConvert.DeserializeObject<List<EffectInfo>>(test);
-
-            Debug.Log(result);
         }
-
-        // [NodeAction("AddEffect(Hero)")]
-        // public void AddEffects()
-        // {
-        //     if (EditorApplication.isPlaying == true)
-        //     {
-        //         var mainGamePlayData = ApplicationController.Instance.GetGamePlayController().GetGamePlayData<MainGamePlayData>();
-        //         var effectManager = ApplicationController.Instance.GetMonobehaviourLifeCycle<EffectManager>();
-        //         effectManager.AddRequestedEffects(mainGamePlayData.hero, effects, name);
-        //     }
-        //     else
-        //     {
-        //         EditorUtility.DisplayDialog("Effect Group", "只有 PlayMode 時可以使用", "OK");
-        //     }
-        // }
     }
 }
